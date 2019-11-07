228e85a50bc7c530105e3eb383bebc8d6a1927f5
Ashlee Hayes <hayesashlee88@gmail.com>
2019-11-07 10:32:58 -0500

    finished part1

--- reacthooks/src/Playground.js
+++ reacthooks/src/Playground.js
@@ -2,7 +2,25 @@ import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
 import randomColor from 'randomcolor'
 
 // where we will start using Hooks and then switch to Paint 
+// 1- useState hook
+//useState is a function, takes initial state as parameter
 
 export default function Playground() {
-  return <div className="playground">Hello Worlds!</div>
+  //i've set the initial state to 0 here
+  // returns an array (using array destructuring) of 2 items and naming them at same time
+ // rename this: const [state, setState] = useState(0)
+ // holding state in count- update state in setCount
+ const [count, setCount] = useState(0)
+
+ return (
+   <div>
+     {count}
+     <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+     <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
+   </div>
+ )
+  
 }
+
+// https://cdb.reacttraining.com/react-inline-functions-and-performance-bdff784f5578
+//performance issues of inline functions
