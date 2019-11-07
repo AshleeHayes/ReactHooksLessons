beb462f8d34d83a6b985b2fda63b56e04417258a
Ashlee Hayes <hayesashlee88@gmail.com>
2019-11-07 09:59:32 -0500

    first

---
+++ reacthooks/src/Playground.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
+import randomColor from 'randomcolor'
+
+// where we will start using Hooks and then switch to Paint 
+
+export default function Playground() {
+  return <div className="playground">Hello Worlds!</div>
+}
